7446ef1d9dc0113d56ed6926772588630ad25e56
gsrr <jerrycheng1128@gmail.com>
2015-12-08 12:58:57 +0800

    init parse web server

---
+++ websever/kmdn/cloud/main.js
@@ -0,0 +1,208 @@
+
+function initParse()
+{
+    Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+}
+
+function query_find(InfoName, key_comp, value)
+{
+    initParse();
+    var info = Parse.Object.extend(InfoName);
+    var query = new Parse.Query(info);
+    query.equalTo(key_comp, value);
+    return query;
+}
+
+function query_all(InfoName)
+{
+    initParse();
+    var info = Parse.Object.extend(InfoName);
+    var query = new Parse.Query(info);
+    return query;
+}
+
+Parse.Cloud.define("getGoods", function(request, response) {
+    
+    var info = require('cloud/info.js');
+    query = query_find("GoodInfo", "store", request.params.store);
+    info.getInfo(request,response, query)
+});
+
+Parse.Cloud.define("getAllStore", function(request, response) {
+    
+    query = query_all("StoreInfo");
+    query.find({
+      success: function(data) {
+        ret = {
+            "status" : 0,
+            "data" : data
+        }
+        response.success(data);
+      },
+      error: function(error) {
+        ret = { 
+            "status" : -1,
+            "msg" : "Error: " + error.code + " " + error.message
+        };
+        response.error(ret);
+      }
+    });
+});
+
+Parse.Cloud.define("getAllNews", function(request, response) {
+    
+    query = query_find("NewsInfo", "date", request.params.date);
+    query.find({
+      success: function(data) {
+        ret = {
+            "status" : 0,
+            "data" : data
+        }
+        response.success(data);
+      },
+      error: function(error) {
+        ret = { 
+            "status" : -1,
+            "msg" : "Error: " + error.code + " " + error.message
+        };
+        response.error(ret);
+      }
+    });
+});
+
+Parse.Cloud.define("getPoint", function(request, response) {
+    
+    query = query_find("KmdnInfo", "feature", request.params.feature);
+    query.find({
+      success: function(data) {
+        ret = { 
+            "status" : 0,
+            "data" : data[0].get("point")
+        };
+        response.success(ret);
+      },
+      error: function(error) {
+        ret = { 
+            "status" : -1,
+            "msg" : "Error: " + error.code + " " + error.message
+        };
+        response.error(ret);
+      }
+    });
+});
+
+
+
+Parse.Cloud.define("test_module", function(request, response) {
+    var test = require('cloud/test.js');
+    response.success(test.test());
+});
+
+Parse.Cloud.define("test_module_addPoints", function(request, response) {
+    
+    var test = require('cloud/test.js');
+    test.addPoints(request,response)
+});
+
+
+
+
+Parse.Cloud.define("queryBusinessPoint", function(request, response) {
+    Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+    var GameScore = Parse.Object.extend("BusinessPoint");
+    var query = new Parse.Query(GameScore);
+    query.equalTo("name", request.params.name);
+    query.find({
+      success: function(results) {
+        var GameScore = Parse.Object.extend("User");
+        var query = new Parse.Query(GameScore);
+        query.get(request.params.user, {
+          success: function(gameScore) {
+            gameScore.set("points", gameScore.get("points") + results[0].get("points"));
+            gameScore.save();
+            response.success({"total" : gameScore.get("points"), "result" : results});
+          },
+          error: function(object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and message.
+            response.error( "add points:" + error)
+          }
+        });
+      },
+      error: function(error) {
+        response.error("Error: " + error.code + " " + error.message);
+      }
+    });
+});
+
+Parse.Cloud.define("createBusinessPoint", function(request, response) {
+    Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+    var GameScore = Parse.Object.extend("BusinessPoint");
+    var gameScore = new GameScore();
+    gameScore.set("name", request.params.name);
+    gameScore.set("points", request.params.points);
+    gameScore.save(null, {
+      success: function(gameScore) { 
+        response.success(gameScore);
+      },
+      error: function(gameScore, error) {
+        response.error('Failed to create new object, with error code: ' + error.message);
+      } 
+    });
+});
+
+Parse.Cloud.define("initUserPoint", function(request, response) {
+    Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+    var GameScore = Parse.Object.extend("User");
+    var query = new Parse.Query(GameScore);
+    query.get(request.params.user, {
+      success: function(gameScore) {
+        gameScore.set("points", 15);
+        gameScore.save();
+        response.success(gameScore);
+      },
+      error: function(object, error) {
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and message.
+      }
+    });
+    
+});
+
+Parse.Cloud.define("createUser", function(request, response) {
+    Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+    var GameScore = Parse.Object.extend("UserInfo");
+    var gameScore = new GameScore();
+    
+    gameScore.set("user", request['user']);
+    gameScore.set("points", 0);
+    gameScore.save(null, {
+      success: function(gameScore) { 
+        response.success('New object created with objectId: ' + gameScore.id + ' ' + gameScore.points);
+      },
+      error: function(gameScore, error) {
+        response.error('Failed to create new object, with error code: ' + error.message);
+      } 
+    });
+    
+});
+
+Parse.Cloud.define("queryUser", function(request, response) {
+  Parse.initialize("mpuTWZgtQanqfCdO8IWJEJbHTZoQq97h6pG2qhGT", "2Xpjqqtz71ab31Hp9jHRARW7t7jaaBjjnZRJy77o");
+    var GameScore = Parse.Object.extend("UserInfo");
+    var query = new Parse.Query(GameScore);
+    query.equalTo(request.params.key, request.params.value);
+    query.find({
+      success: function(results) {
+        response.success(results);
+      },
+      error: function(error) {
+        response.error("Error: " + error.code + " " + error.message);
+      }
+    });
+});
+
+Parse.Cloud.define("test", function(request, response) {
+    var key = request["key"];
+  response.success({"status" : 0, "user" : key});
+});
---
+++ websever/kmdn/public/js/kmdn.js
@@ -0,0 +1,115 @@
+﻿
+
+
+function Kmdn() {}
+
+
+Kmdn.prototype.openMovie = function()
+{
+    function cbk_movie(data)
+    {
+        console.log(data);
+        for(var i = 0 ; i < data.length ; i++)
+        {
+            console.log(data[i].get("good"));
+            console.log(data[i].get("point"));
+        }
+    }
+    callCloud("getGoods", {"store":"kmdn_movie"}, cbk_movie);
+}
+
+Kmdn.prototype.openStore = function()
+{
+    function cbk_store(data)
+    {
+        console.log(data);
+        for(var i = 0 ; i < data.length ; i++)
+        {
+            console.log(data[i].get("name"));
+            console.log(data[i].get("realName"));
+        }
+    }
+    callCloud("getAllStore", {}, cbk_store);
+}
+
+Kmdn.prototype.openNews = function()
+{
+    function cbk_news(data)
+    {
+        function show_news(data)
+        {
+            for(var i = 0 ; i < data.length ; i++)
+            {
+                console.log(data[i].get("author"));
+                console.log(data[i].get("title"));
+                console.log(data[i].get("date"));
+                console.log(data[i].get("content"));
+            }
+        }
+        console.log(data);
+        var d = new Date();
+        var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + "05";
+        console.log(date);
+        callCloud("getAllNews", {'date' : date}, show_news);
+    }
+    alert ('get point of news');
+    var paraList = {
+        'feature' : "news",
+    }
+    callCloud("getPoint", paraList, cbk_news);
+};
+
+function open_news_old()
+{
+    function test_module_ret(data)
+    {
+        alert(data);
+    }
+    function test_module()
+    {
+        callCloud("test_module", {}, test_module_ret);
+    }
+    function test_module_addPoints_ret(data)
+    {
+        console.log(data);
+    }
+     function test_module_addPoints(paraList)
+    {
+        alert("test_module_addPoints");
+        console.log(paraList);
+        callCloud("test_module_addPoints", paraList, test_module_addPoints_ret);
+    }
+    function createBusinessPoint_ret(data)
+    {
+        console.log(data);
+    }
+    function createBusinessPoint()
+    {
+        var paraList = {
+            'name' : "kmdn",
+            'points' : 15,
+        }
+        callCloud("createBusinessPoint", paraList, createBusinessPoint_ret);
+    }
+    function queryBusinessPoint_ret(data)
+    {
+        var total = data['total']
+        var result = data["result"];
+        
+        alert("You have get " + result[0].get("points"));
+        $("#user_points").html("點數:" + total)
+    }
+    function queryBusinessPoint(paraList)
+    {
+        console.log(paraList);
+        callCloud("queryBusinessPoint", paraList, queryBusinessPoint_ret);
+    }
+    alert("kmdn_news");
+    //createBusinessPoint();
+    var paraList = {
+        'user' : $("#user").html(),
+        'name' : "kmdn",
+    }
+    queryBusinessPoint(paraList)
+    //test_module_addPoints(paraList)
+}
